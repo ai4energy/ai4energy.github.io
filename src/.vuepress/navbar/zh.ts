1ca4e55b61d701eca221c7887416bc0cf07fba70
Mingtao Li <mingtao@xjtu.edu.cn>
2023-10-05 19:41:07 +0800

    修改导航栏

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,10 +3,43 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/",
   {
+    text: "导论课程",
+    icon: "book",
+    link: "/docs/",
+  },
+  {
+    text: "路线图",
+    icon: "book",
+    link: "/docs/",
+  },
+  {
     text: "Ai4E文档",
     icon: "book",
     link: "/docs/",
   },
-
-
+  {
+    text: "虚拟实验室",
+    icon: "book",
+    link: "/docs/",
+  },
+  {
+    text: "组件库",
+    icon: "book",
+    link: "/docs/",
+  },
+  {
+    text: "碳中和开源汇编",
+    icon: "book",
+    link: "/docs/",
+  },
+  {
+    text: "新闻",
+    icon: "book",
+    link: "/docs/",
+  },
+  {
+    text: "源码",
+    icon: "book",
+    link: "/docs/",
+  },
 ]);
