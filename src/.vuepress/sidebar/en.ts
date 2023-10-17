25f6979cd08c3498757b42440064b4703ab7442a
Mingtao Li <mingtao@xjtu.edu.cn>
2023-10-17 09:21:53 +0800

    修改英文的侧边栏

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,21 +1,8 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const enSidebar = sidebar({
-  "/": [
+  "/en/": [
     "",
-    {
-      text: "Demo",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "Docs",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
     "slides",
   ],
 });
